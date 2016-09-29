b3ffce462d1f9b7bcd4938a5840e9a36a852530d
Himanshu Choudhary <himanshuchoudhary@live.com>
2016-09-29 20:44:09 +0530

    Modified lstm classifier. Added weights and created vocab with both train & test sets

--- project/classifiers/LSTMClassifier.py
+++ project/classifiers/LSTMClassifier.py
@@ -1,5 +1,4 @@
 
-import pandas as pd
 import numpy as np
 from nltk import word_tokenize
 from keras.preprocessing.sequence import pad_sequences
@@ -7,38 +6,42 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, Embedding
 
 class LSTMReviewAnalyzer():
-    def __init__(self):
-        # self.vocab = None
-        pass
+
+    def __init__(self,vocab,max_review_len=100):
+        self.max_review_len = max_review_len
+        self.vocab = vocab
+        self.model = Sequential()
+        self.model.add(Embedding(len(self.vocab) + 1, 128, input_length=max_review_len, dropout=0.2))
+        self.model.add(LSTM(128, dropout_W=0.2, dropout_U=0.2))
+        self.model.add(Dense(1, activation='tanh'))
+        self.model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
 
     def __indexize(self,token):
         try:
             return self.vocab[self.vocab['token'] == token].index[0]
         except IndexError:
-            return len(self.vocab)
+            return 0
 
     def __pad(self,sequence):
         # return pad_sequences(sequence,self.max_review_len)
         return sequence[0:self.max_review_len] if len(sequence) > self.max_review_len else np.concatenate(
             (sequence, np.zeros(self.max_review_len - len(sequence), dtype=np.int32)))
 
-    def train(self,reviews,ratings,max_review_len=100):
-        self.max_review_len = max_review_len
+    def __weight(self,rating):
+        return 2 if rating in [-1,-.5] else 1
+
+    def train(self,reviews,ratings):
         reviews = map(word_tokenize,reviews)
-        tokens = []
-        for review in reviews:
-            tokens.extend(review)
-        self.vocab = pd.DataFrame(list(set(tokens)), columns=['token'])
         for i in range(len(reviews)):
             reviews[i] = self.__pad(map(self.__indexize,reviews[i]))
         reviews = np.array(reviews)
         ratings = (ratings-3)/2.0
-        self.model = Sequential()
-        self.model.add(Embedding(len(self.vocab)+1, 128, input_length=max_review_len, dropout=0.2))
-        self.model.add(LSTM(128, dropout_W=0.2, dropout_U=0.2))
-        self.model.add(Dense(1, activation='sigmoid'))
-        self.model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-        self.model.fit(reviews, ratings, nb_epoch=10, batch_size=32)
+        # sample_weight = np.asarray(map(lambda x : 5/(2*x + 3), ratings))
+        weights = np.asarray(map(self.__weight, ratings))
+        self.model.fit(reviews, ratings, nb_epoch=100, batch_size=32,sample_weight=weights)
+
+    def summary(self):
+        return self.model.summary()
 
     def evaluate(self,reviews,ratings):
         reviews = map(word_tokenize, reviews)
@@ -47,3 +50,9 @@ class LSTMReviewAnalyzer():
         reviews = np.array(reviews)
         ratings = (ratings-3)/2.0
         return self.model.evaluate(reviews, ratings)
+
+    def predict(self,review):
+        review = word_tokenize(review)
+        review = self.__pad(map(self.__indexize,review))
+        review = np.array([review])
+        return self.model.predict(review)[0][0]
--- project/tests/test_rating_lstm.py
+++ project/tests/test_rating_lstm.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 from ..classifiers.LSTMClassifier import LSTMReviewAnalyzer
 from .. import Utils
 import numpy as np
+import pandas as pd
+from nltk import word_tokenize
 from sklearn.cross_validation import train_test_split
 
 np.random.seed(7)
@@ -11,11 +13,27 @@ dataset = Utils.getDataset(100,random=0)
 reviews = dataset['Text'].values
 ratings = dataset['Score'].values
 
+# reviews = np.array(map(Utils.filterStopwords,reviews))
+
+tokenized_reviews = map(word_tokenize,reviews)
+tokens = []
+for review in tokenized_reviews:
+    tokens.extend(review)
+vocab = pd.DataFrame(list(set(tokens)), columns=['token'])
+vocab.index += 1
+
 train_reviews, test_reviews, train_ratings, test_ratings = train_test_split(reviews,ratings,test_size=.2)
 
-analyzer = LSTMReviewAnalyzer()
+analyzer = LSTMReviewAnalyzer(vocab)
 
 analyzer.train(train_reviews,train_ratings)
+
+print("Testing Set -------- ",file=Utils.FILE_OUT)
+for i in range(len(test_reviews)):
+    print(test_ratings[i],analyzer.predict(test_reviews[i]),file=Utils.FILE_OUT)
+print("Training Set ---------- ",file=Utils.FILE_OUT)
+for i in range(len(train_reviews)):
+    print(train_ratings[i],analyzer.predict(train_reviews[i]),file=Utils.FILE_OUT)
 
 score = analyzer.evaluate(test_reviews,test_ratings)
 print("# of reviews in dataset : {:d}".format(len(dataset)),file=Utils.FILE_OUT)

7cf51d50776f718c0a5dbb208afec16e52f6f198
Himanshu Choudhary <himanshuchoudhary@live.com>
2016-10-06 23:14:50 +0530

    Removed prediction printing for both train & test set.

--- project/tests/test_rating_lstm.py
+++ project/tests/test_rating_lstm.py
@@ -11,7 +11,7 @@ np.random.seed(7)
 
 dataset = Utils.getDataset(100,random=0)
 reviews = dataset['Text'].values
-ratings = dataset['Score'].values
+ratings = dataset['Score']
 
 # reviews = np.array(map(Utils.filterStopwords,reviews))
 
@@ -21,21 +21,17 @@ for review in tokenized_reviews:
     tokens.extend(review)
 vocab = pd.DataFrame(list(set(tokens)), columns=['token'])
 vocab.index += 1
-
 train_reviews, test_reviews, train_ratings, test_ratings = train_test_split(reviews,ratings,test_size=.2)
 
-analyzer = LSTMReviewAnalyzer(vocab)
+indices = test_ratings.index
+test_ratings = test_ratings.values
+train_ratings = train_ratings.values
 
+analyzer = LSTMReviewAnalyzer(vocab)
 analyzer.train(train_reviews,train_ratings)
 
-print("Testing Set -------- ",file=Utils.FILE_OUT)
-for i in range(len(test_reviews)):
-    print(test_ratings[i],analyzer.predict(test_reviews[i]),file=Utils.FILE_OUT)
-print("Training Set ---------- ",file=Utils.FILE_OUT)
-for i in range(len(train_reviews)):
-    print(train_ratings[i],analyzer.predict(train_reviews[i]),file=Utils.FILE_OUT)
-
 score = analyzer.evaluate(test_reviews,test_ratings)
+print("Vocab size : {:d}".format(len(vocab)),file=Utils.FILE_OUT)
 print("# of reviews in dataset : {:d}".format(len(dataset)),file=Utils.FILE_OUT)
 print("# of reviews in training set : {:d}".format(len(train_reviews)),file=Utils.FILE_OUT)
 print("# of reviews in testing set : {:d}".format(len(test_reviews)),file=Utils.FILE_OUT)

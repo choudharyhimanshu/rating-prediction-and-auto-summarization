062e398858fb8c5972c3564aee05e26945bc57fc
Himanshu Choudhary <himanshuchoudhary@live.com>
2016-10-07 20:12:58 +0530

    Updated word2vec.train. Added build_vocab.

--- project/word2vec/train.py
+++ project/word2vec/train.py
@@ -1,11 +1,13 @@
 
 import pandas as pd
 import numpy as np
+import codecs
 
 from gensim.models import Word2Vec
 
 NROWS = 1000
 FILE_COUNT = 'local/word2vec/count.txt'
+FILE_VOCAB = 'local/word2vec/reviews.vocab'
 FILE_MODEL = 'local/word2vec/reviews.word2vec.model'
 FILE_DATASET = 'project/dataset/Reviews.csv'
 
@@ -13,7 +15,7 @@ def getDataset(offset):
     file = open(FILE_DATASET,'r')
     return pd.read_csv(file, header=0, index_col=0, usecols=['Id', 'Text'], skiprows=range(1,offset), nrows=NROWS, encoding='utf-8')
 
-with open(FILE_COUNT,'r') as file:
+with codecs.open(FILE_COUNT,'r',encoding='utf-8') as file:
     OFFSET = int(file.read())
     file.close()
 
@@ -21,12 +23,18 @@ dataset = getDataset(OFFSET)
 
 try:
     model = Word2Vec.load(FILE_MODEL)
+    # model = Word2Vec.load_word2vec_format(FILE_MODEL,binary=False)
 except IOError:
+    with codecs.open(FILE_VOCAB, 'r', encoding='utf-8') as file:
+        vocab = file.read().splitlines()
+        file.close()
     model = Word2Vec(size=40, window=2, workers=1, min_count=1)
+    model.build_vocab([vocab])
 
 model.train(dataset.Text)
 
 model.save(FILE_MODEL)
+# model.save_word2vec_format(FILE_MODEL,binary=False)
 
 with open(FILE_COUNT,'w') as file:
     file.write(str(OFFSET+NROWS))
